d81e616f6eb8609d47108bc172134e6f23e7c33a
Konrad Knecht <konrad.knecht@gmail.com>
2020-04-21 16:03:02 +0100

    .

---
+++ Game engine lab test/Assets/AI_Follow.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class AI_Follow : MonoBehaviour
+{
+    private GameObject[] lights;
+    private GameObject targetLight;
+    private GameObject lastTargetLight;
+
+    UnityEngine.AI.NavMeshAgent MyNavMesh;
+    void Start()
+    {
+        SetTargetLight();
+        MyNavMesh = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    bool SetTargetLight()
+    {
+        lights = GameObject.FindGameObjectsWithTag("Green");
+        lights = lights.Where(l => !l.Equals(lastTargetLight)).ToArray();
+        if (lights.Length == 0)
+        {
+            return false;
+        }
+
+        lastTargetLight = targetLight;
+        
+
+        targetLight = lights[Random.Range(0, lights.Length)];
+
+        return true;
+    }
+    void Update()
+    {
+
+        if (lastTargetLight != null && targetLight.Equals(lastTargetLight))
+        {
+            if (SetTargetLight()) FollowLight();
+        }
+        else if (targetLight !=null || SetTargetLight())
+        {
+            if (!targetLight.CompareTag("Green") && !SetTargetLight())
+            {
+                return;
+            }
+            FollowLight();
+        }
+    }
+
+    void FollowLight()
+    {
+        var targetRotation = Quaternion.LookRotation(targetLight.transform.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 5 * Time.deltaTime);
+        MyNavMesh.destination = targetLight.transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == targetLight)
+        {
+            SetTargetLight();
+        }
+    }
+}
---
+++ Game engine lab test/Assets/LightProperties.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightProperties : MonoBehaviour
+{
+    Color[] colors = new Color[3];
+    int ColInd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        colors[0] = Color.green;
+        colors[1] = Color.red;
+        colors[2] = Color.blue;
+        StartCol();
+        StartCoroutine(Time());
+    }
+
+    void StartCol()
+    {
+        ColInd = Random.Range(0, colors.Length);
+        Color CurrCol = colors[ColInd];
+        gameObject.GetComponent<Renderer>().material.color = CurrCol;
+    }
+    void ChangeCol(int ind)
+    {
+        if(ind == 0)
+        {
+            Color CurrCol = colors[1];
+            ColInd = 1;
+            gameObject.GetComponent<Renderer>().material.color = CurrCol;
+        }
+        if (ind == 1)
+        {
+            Color CurrCol = colors[2];
+            ColInd = 2;
+            gameObject.GetComponent<Renderer>().material.color = CurrCol;
+        }
+        if (ind == 2)
+        {
+            Color CurrCol = colors[0];
+            ColInd = 0;
+            gameObject.GetComponent<Renderer>().material.color = CurrCol;
+        }
+    }
+
+    IEnumerator Time()
+    {
+        int RandTime;
+        if (ColInd == 0)
+        {
+            gameObject.name = "Green";
+            gameObject.tag = "Green";
+            RandTime = Random.Range(5, 10);
+            yield return new WaitForSeconds(RandTime);
+            ChangeCol(ColInd);
+        }
+        if (ColInd == 1)
+        {
+            gameObject.name = "Red";
+            gameObject.tag = "Null";
+            RandTime = Random.Range(5, 10);
+            yield return new WaitForSeconds(RandTime);
+            ChangeCol(ColInd);
+        }
+        if (ColInd == 2)
+        {
+            gameObject.name = "Blue";
+            gameObject.tag = "Null";
+            yield return new WaitForSeconds(4);
+            ChangeCol(ColInd);
+        }
+        StartCoroutine(Time());
+    }
+}
